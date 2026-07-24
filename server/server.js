313aa5060976447c5b436e4d13a79153e97cdbb4
Muhammad Usman <usman.rauf.953@gmail.com>
2026-07-25 02:21:50 +0500

    update server.js

--- server/server.js
+++ server/server.js
@@ -1,13 +1,11 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+
 import connectDB from "./config/db.js";
 import studentRoutes from "./routes/studentRoutes.js";
 
 dotenv.config();
-
-// Connect Database
-connectDB();
 
 const app = express();
 
@@ -17,9 +15,24 @@ app.use(
       "http://localhost:5173",
       "https://studentms-frontend-opal.vercel.app",
     ],
+    credentials: true,
   }),
 );
+
 app.use(express.json());
+
+// ✅ Ensure DB connection before every request
+app.use(async (req, res, next) => {
+  try {
+    await connectDB();
+    next();
+  } catch (error) {
+    res
+      .status(500)
+      .json({ success: false, error: "Database connection failed" });
+  }
+});
+
 app.use("/api/students", studentRoutes);
 
 app.get("/", (req, res) => {
@@ -28,13 +41,5 @@ app.get("/", (req, res) => {
     message: "Student Management API Running 🚀",
   });
 });
-
-if (process.env.NODE_ENV !== "production") {
-  const PORT = process.env.PORT || 5000;
-
-  app.listen(PORT, () => {
-    console.log(`🚀 Server running on port ${PORT}`);
-  });
-}
 
 export default app;
