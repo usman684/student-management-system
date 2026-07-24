fe8e17a2a9af933d5d8f919001b0805b09abaada
Muhammad Usman <usman.rauf.953@gmail.com>
2026-07-25 01:41:36 +0500

    update

--- server/server.js
+++ server/server.js
@@ -11,7 +11,9 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors(
+    origin: "https://studentms-frontend-opal.vercel.app"
+));
 app.use(express.json());
 app.use("/api/students", studentRoutes);
 
